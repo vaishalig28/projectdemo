cc12fc924e640e11d4229ec4a1681417386b7c3f
root <v@kshita16>
2018-09-10 11:11:00 +0530

    hello

---
+++ projectdemo.py
@@ -0,0 +1,14 @@
+from flask import Flask
+import os
+
+app=Flask(__name__)
+
+port=int(os.getenv("PORT"))
+
+@app.route('/')
+def hello_world():
+    return 'Welcome'
+
+if __name__=='__main__':
+    #port=1234
+    app.run(host='0.0.0.0',port=port)
